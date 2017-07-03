f98beab5934439b4383e07b820d360379956f2d1
Kristie Huang <kristiehuang01@gmail.com>
2017-07-03 09:40:49 -0700

    Initial commit

---
+++ Time-Scheduling-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time-Scheduling-App
+//
+//  Created by Kristie Huang on 7/2/17.
+//  Copyright © 2017 Kristie Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
