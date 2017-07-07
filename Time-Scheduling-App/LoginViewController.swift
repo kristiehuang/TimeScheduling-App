82410cb7b1108d69faece78b909f3f2f92b2a3b4
Kristie Huang <kristiehuang01@gmail.com>
2017-07-07 16:50:38 -0700

    Basic UI + Add basic auth

--- Time-Scheduling-App/LoginViewController.swift
+++ Time-Scheduling-App/LoginViewController.swift
@@ -8,11 +8,38 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        print("login button tapped")
+        
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        authUI.delegate = self
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+    }
+    //google login doesnt work yet
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        print("handle user signup / login")
+    }
 }
